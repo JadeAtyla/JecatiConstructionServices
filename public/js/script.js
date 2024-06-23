2a9bd91afd9cf09287be06f875f4f9867b318d6c
contentmoderated <estores.christian187@gmail.com>
2024-06-23 14:10:08 +0800

    added popups to pages

--- public/js/script.js
+++ public/js/script.js
@@ -4,8 +4,8 @@ function contactUsRedirect() {
   window.location.replace('/contact-us');
 }
 function inquireNow(element) {
-  element[0].style.visibility = 'visible';
+  element[0].style.display = 'block';
 }
 function submitInquiry(element) {
-  element[0].style.visibility = 'hidden';
+  element[0].style.display = 'none';
 }
